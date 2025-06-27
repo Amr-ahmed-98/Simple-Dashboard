c7a1b5be336c0058f97917095e7dab6e0a288826
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-06-27 17:43:47 +0300

    Fix Chart

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   transpilePackages: ['chart.js', 'react-chartjs-2'],
   webpack: (config: any) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       'chart.js/auto': 'chart.js/dist/chart.js'
     };
-    
-    // Add this to handle canvas (used by Chart.js)
-    config.externals.push({
-      canvas: 'canvas',
-    });
-    
     return config;
   }
 }
-
-module.exports = nextConfig;
